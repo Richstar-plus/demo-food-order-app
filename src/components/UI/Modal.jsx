fd3951b081239f11b3d167bf4d6df137c7c8fe26
richard <richardsunday0812@gmail.com>
2026-03-10 13:17:38 +0100

    Add Modal component for displaying modal dialogs

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+import { ccreatePortal } from "react-dom";
+
+export function Modal({ children, open, className = '' }) {
+  const dialog = useRef();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal"),
+  );
+}
