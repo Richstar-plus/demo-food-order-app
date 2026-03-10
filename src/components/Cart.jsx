a3723c2b106813618b2a3dbb8174962f728a5610
richard <richardsunday0812@gmail.com>
2026-03-10 14:57:33 +0100

    Refactor Cart component to use CartItem for individual cart items; add CartItem component for better structure and functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import UserProgressContext from "../store/UserProgressContext";
 import { Button } from "./UI/Button";
+import { CartItem } from "./CartItem";
 
 export function Cart() {
   const cartContext = useContext(CartContext);
@@ -16,14 +17,21 @@ export function Cart() {
   function handleCloseCart() {
     userProgressContext.hideCart();
   }
+
+  
   return (
     <Modal className="cart" open={userProgressContext.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartContext.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartContext.addItemToCartHandler(item)}
+            onDecrease={() => cartContext.removeItemFromCartHandler(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">Total: {currencyFormatter.format(cartTotal)}</p>
